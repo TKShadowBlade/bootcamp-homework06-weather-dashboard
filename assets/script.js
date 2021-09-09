fb7fd9e1e79b75ac945c850a794f7c4b0d74ab93
TKShadowBlade <katanablack85@gmail.com>
2021-09-09 17:00:45 -0400

    init commit for html, css and scripting

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+// Global variables
+var searchHistory = [];
+
+
+// API variables
+var weatherApiKey = "98c3f241443d3fbc1d57941dfa09a89e"
+var apiUrl = "${weatherApiRootUrl}/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely,hourly&appid=${weatherApiKey}";
+
+
+
